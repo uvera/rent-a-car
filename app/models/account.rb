1667b0db18bf8dc3007b984d1f37be633d8f01a4
Dušan <dusan.uveric@mitigate.dev>
2023-01-16 20:33:32 +0100

    Add account annnotation
    Signed-off-by: Dušan <dusan.uveric@mitigate.dev>

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,3 +2,18 @@ class Account < ApplicationRecord
   include Rodauth::Rails.model
   enum :status, unverified: 1, verified: 2, closed: 3
 end
+
+# == Schema Information
+#
+# Table name: accounts
+#
+#  id            :bigint           not null, primary key
+#  email         :string           not null
+#  kind          :string           default("main"), not null
+#  password_hash :string
+#  status        :integer          default("unverified"), not null
+#
+# Indexes
+#
+#  index_accounts_on_email  (email) UNIQUE WHERE (status = ANY (ARRAY[1, 2]))
+#
