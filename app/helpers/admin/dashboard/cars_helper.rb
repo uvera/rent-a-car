025a34cb071a066114ee40e7182e3dc3f8149c89
Dušan <dusan.uveric@mitigate.dev>
2023-01-23 13:53:36 +0100

    feature: app: common helper for cars
    Signed-off-by: Dušan <dusan.uveric@mitigate.dev>

--- app/helpers/admin/dashboard/cars_helper.rb
+++ app/helpers/admin/dashboard/cars_helper.rb
@@ -1,39 +1,6 @@
 module Admin
   module Dashboard
     module CarsHelper
-      def car_brands_for_select
-        Car::AVAILABLE_CAR_BRANDS.map do |brand|
-          [I18n.t("cars.brands.#{brand}"), brand.to_s]
-        end
-      end
-
-      def car_engine_types_for_select
-        Car::AVAILABLE_CAR_ENGINE_TYPES.map do |engine|
-          [I18n.t("cars.engine_types.#{engine}"), engine.to_s]
-        end
-      end
-
-      def car_transmission_types_for_select
-        Car::AVAILABLE_CAR_TRANSM_TYPES.map do |engine|
-          [I18n.t("cars.transmission_types.#{engine}"), engine.to_s]
-        end
-      end
-
-      def car_body_configuration_types_for_select
-        Car::AVAILABLE_CAR_BODY_CONF.map do |body|
-          [I18n.t("cars.body_types.#{body}"), body.to_s]
-        end
-      end
-
-      def car_sortables_for_select
-        %i[price_in_eur release_date name deposit].map do |field|
-          %i[asc desc].map do |order|
-            ["#{I18n.t("forms.cars.sorting.fields.#{field}")} #{I18n.t("forms.cars.sorting.#{order}")}",
-             "#{field} #{order}"]
-          end
-        end.flatten(1)
-      end
-
       # param [Car] car
       def car_images_upload_input(car)
         react_component 'Common.Forms.ImagesInput', props: {
