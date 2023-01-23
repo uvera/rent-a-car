139528ac5255852dadc87a4406e6f5a76ce7429e
Dušan <dusan.uveric@mitigate.dev>
2023-01-23 15:07:35 +0100

    feature: app: rodauth navbar for admin
    Signed-off-by: Dušan <dusan.uveric@mitigate.dev>

--- app/controllers/admin/rodauth_controller.rb
+++ app/controllers/admin/rodauth_controller.rb
@@ -1,4 +1,5 @@
 class Admin::RodauthController < ApplicationController
+  layout 'admin/rodauth/application'
   # used by Rodauth for rendering views, CSRF protection, and running any
   # registered action callbacks and rescue_from handlers
 end
