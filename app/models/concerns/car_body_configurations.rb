dfa9320132238a50b7857f8f4e0325a4a333e5c8
Dušan <dusan.uveric@mitigate.dev>
2023-01-23 17:25:35 +0100

    feature: cars: van body configuration
    Signed-off-by: Dušan <dusan.uveric@mitigate.dev>

--- app/models/concerns/car_body_configurations.rb
+++ app/models/concerns/car_body_configurations.rb
@@ -3,6 +3,6 @@
 module CarBodyConfigurations
   extend ActiveSupport::Concern
 
-  AVAILABLE_CAR_BODY_CONF = %i[hatchback sedan convertible suv minivan coupe].freeze
+  AVAILABLE_CAR_BODY_CONF = %i[hatchback sedan convertible suv minivan coupe van].freeze
   AVAILABLE_CAR_BODY_CONF_ENUM_HASH = AVAILABLE_CAR_BODY_CONF.index_by(&:itself).transform_values(&:to_s)
 end
