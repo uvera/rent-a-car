061999273646b2ca4ba58bf8c2eab6c584f6e7da
Dušan <dusan.uveric@mitigate.dev>
2023-01-23 17:33:53 +0100

    feature: app/cars: more car brands
    Signed-off-by: Dušan <dusan.uveric@mitigate.dev>

--- app/models/concerns/car_brands.rb
+++ app/models/concerns/car_brands.rb
@@ -1,6 +1,10 @@
 module CarBrands
   extend ActiveSupport::Concern
 
-  AVAILABLE_CAR_BRANDS = %i[audi bmw].freeze
+  AVAILABLE_CAR_BRANDS = %i[audi bmw tesla mazda volkswagen skoda seat
+                            peugeot renault mercedes toyota hyundai porsche chevrolet nissan kia gmc
+                            ford volvo opel citroen suzuki dacia fiat jeep chrysler mini
+                            land_rover lexus dodge polestar
+].freeze
   AVAILABLE_CAR_BRANDS_ENUM_HASH = AVAILABLE_CAR_BRANDS.index_by(&:itself).transform_values(&:to_s)
 end
