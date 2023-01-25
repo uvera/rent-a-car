82ad4040db346ce3d5ece897c03281f28a142763
Dušan <dusan.uveric@mitigate.dev>
2023-01-25 20:14:04 +0100

    feature: app: lazy load react component
    Signed-off-by: Dušan <dusan.uveric@mitigate.dev>

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,34 +14,40 @@
 // const images = require.context('./images', true)
 // const imagePath = (name) => images(name, true)
 
-import React from "react";
+import React, { lazy } from "react";
 import "tw-elements";
 import * as Flowbite from "flowbite";
 import "@hotwired/turbo-rails";
 import ReactOnRails from "react-on-rails";
 import TagInput from "../components/common/./forms/tagInput";
 import ToastDismisser from "../components/common/configurations/toastDismisser";
-import { ImagesInput } from "../components/common/forms/imagesInput";
-import MultiSelect from "../components/common/forms/multiSelect";
-import SingleSelect from "../components/common/forms/singleSelect";
 import { debounce } from "../util/debounce";
 import { ToastCloseButton } from "../components/common/toastCloseButton";
-import TipTapInput from "../components/common/forms/tiptap/tipTapInput";
 import ScrollToTop from "../components/common/scrollToTop";
 import { ScrollDisabler as NavbarScrollDisabler } from "../components/common/navbar/scrollDisabler";
+import { ImagesInput } from "../components/common/forms/imagesInput";
 
 const components = {
   TagInput,
   "Configuration.ToastDismisser": ToastDismisser,
-  "Common.Forms.ImagesInput": ImagesInput,
-  "Common.Forms.MultiSelect": MultiSelect,
-  "Common.Forms.SingleSelect": SingleSelect,
+  "Common.Forms.ImagesInput": lazy(() =>
+    import("../components/common/forms/imagesInput").then(
+      ({ ImagesInput }) => ({ default: ImagesInput })
+    )
+  ),
+  "Common.Forms.MultiSelect": lazy(() =>
+    import("../components/common/forms/multiSelect")
+  ),
+  "Common.Forms.SingleSelect": lazy(() =>
+    import("../components/common/forms/singleSelect")
+  ),
   "Common.ToastCloseButton": ToastCloseButton,
-  "Common.Forms.TipTap": TipTapInput,
+  "Common.Forms.TipTap": lazy(() =>
+    import("../components/common/forms/tiptap/tipTapInput")
+  ),
   "Common.Navbar.ScrollDisabler": NavbarScrollDisabler,
   "Common.ScrollToTop": ScrollToTop,
 };
-
 ReactOnRails.register(components);
 ReactOnRails.setOptions({ turbo: true });
 
