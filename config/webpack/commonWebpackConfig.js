9a120fb2d1a01b50bda01163d89dc82dabeb3a95
Dušan <dusan.uveric@mitigate.dev>
2023-01-29 20:35:08 +0100

    webpacker: update config
    Signed-off-by: Dušan <dusan.uveric@mitigate.dev>

--- config/webpack/commonWebpackConfig.js
+++ config/webpack/commonWebpackConfig.js
@@ -1,16 +1,20 @@
-// The source code including full typescript support is available at: 
+// The source code including full typescript support is available at:
 // https://github.com/shakacode/react_on_rails_demo_ssr_hmr/blob/master/config/webpack/commonWebpackConfig.js
 
 // Common configuration applying to client and server configuration
-const { webpackConfig: baseClientWebpackConfig, merge } = require('shakapacker');
+const {
+  webpackConfig: baseClientWebpackConfig,
+  merge,
+} = require("shakapacker");
 
 const commonOptions = {
   resolve: {
-    extensions: ['.css','.scss', '.ts', '.tsx'],
+    extensions: [".css", ".scss", ".ts", ".tsx", ".js", ".jsx"],
   },
 };
 
 // Copy the object using merge b/c the baseClientWebpackConfig and commonOptions are mutable globals
-const commonWebpackConfig = () => merge({}, baseClientWebpackConfig, commonOptions);
+const commonWebpackConfig = () =>
+  merge({}, baseClientWebpackConfig, commonOptions);
 
 module.exports = commonWebpackConfig;
