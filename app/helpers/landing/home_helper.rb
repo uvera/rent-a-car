bbb7b8d4ee1ca99ec6ce2c9c165e6c05ad6642d6
Dušan Uverić <dusanuveric@protonmail.com>
2023-02-09 23:14:43 +0100

    feature: refactor logic for tiptap image injection
    Signed-off-by: Dušan Uverić <dusanuveric@protonmail.com>

--- app/helpers/landing/home_helper.rb
+++ app/helpers/landing/home_helper.rb
@@ -1,6 +1,6 @@
 module Landing
   module HomeHelper
-    ATTACHMENT_REGEX = /{{(image:)(\d+):(.*)}}/
+    ATTACHMENT_REGEX = /{{(image:)(\d+)(:.*)?}}/
 
     # @param [String] key
     def render_tiptap_configuration(key)
@@ -10,10 +10,13 @@ module Landing
       value = (configuration.value || {})[I18n.locale] || configuration.value[:en] || ''
       content_tag(:div, class: 'prose prose-sm') do
         value.gsub(ATTACHMENT_REGEX) do
-          index = Regexp.last_match(2).to_i
-          size = Regexp.last_match(3)
+          match = Regexp.last_match
+          index = match[2].to_i
+          leftover_match = match[3] || ''
+          size = (leftover_match.match(/size=(.*):/).try(:[], 1) || '').sub(/:size=/, '')
+          classes = (leftover_match.match(/class=(.*):/).try(:[], 1) || '').sub(/:class=/, '')
           image = configuration.image_files[index]
-          image_tag(url_for(image), size:, class: 'h-auto max-w-full').html_safe if image
+          image_tag(url_for(image), size:, class: "h-auto max-w-full #{classes}".strip).html_safe if image
         end.html_safe
       end
     end
