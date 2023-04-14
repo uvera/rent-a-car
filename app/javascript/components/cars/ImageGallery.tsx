b43fb920ac637d5bff521e77f54f4876ee840139
Dušan Uverić <dusanuveric@protonmail.com>
2023-04-14 22:59:36 +0200

    image gallery: replace icon with proper expand icon
    Signed-off-by: Dušan Uverić <dusanuveric@protonmail.com>

--- app/javascript/components/cars/ImageGallery.tsx
+++ app/javascript/components/cars/ImageGallery.tsx
@@ -40,20 +40,18 @@ const ImageGallery = ({ images, imageClasses }: ImageGalleryProps) => {
                     onClick={open}
                   >
                     <svg
-                      data-darkreader-inline-stroke=""
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="1.5"
-                      viewBox="0 0 24 24"
                       xmlns="http://www.w3.org/2000/svg"
-                      aria-hidden="true"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
                       className="z-30 w-16 h-16 block text-white bg-gray-300 hover:bg-gray-500 bg-opacity-25 opacity-75 focus:outline-none rounded-lg absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:cursor-pointer"
                     >
                       <path
                         strokeLinecap="round"
                         strokeLinejoin="round"
-                        d="M9 9V4.5M9 9H4.5M9 9L3.75 3.75M9 15v4.5M9 15H4.5M9 15l-5.25 5.25M15 9h4.5M15 9V4.5M15 9l5.25-5.25M15 15h4.5M15 15v4.5m0-4.5l5.25 5.25"
-                      ></path>
+                        d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"
+                      />
                     </svg>
                   </button>
                 </div>
