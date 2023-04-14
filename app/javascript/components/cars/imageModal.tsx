34275d193d4d24a63d34d30ec978b17831b5d623
Dušan Uverić <dusanuveric@protonmail.com>
2023-04-14 22:17:30 +0200

    feature: replace modal with proper lightbox gallery
    Signed-off-by: Dušan Uverić <dusanuveric@protonmail.com>

--- app/javascript/components/cars/imageModal.tsx
+++ app/javascript/components/cars/imageModal.tsx
@@ -1,83 +1,48 @@
-import React, { useRef, useState } from "react";
-import i18n from "../../util/i18n";
-import { Modal } from "flowbite-react";
-import useBodyOverflow from "../../util/useBodyOverflow";
+import React from "react";
+
+import { Gallery, Item } from "react-photoswipe-gallery";
 
 type ImageModalProps = {
-  imgUrl: string;
-  downloadUrl: string;
+  images: Array<[string, { width: number; height: number }]>;
 };
 
-const ImageModal = ({ imgUrl, downloadUrl }: ImageModalProps) => {
-  const [shown, setShown] = useState(false);
-  const [addBodyOverflow, removeBodyOverflow] = useBodyOverflow();
-
-  const ref = useRef<HTMLDivElement | null>(null);
-  const showModal = () => {
-    if (ref.current) {
-      const parent = ref.current.parentElement.parentElement.parentElement;
-      parent.classList.remove("md:h-auto");
-      parent.classList.remove("max-w-5xl");
-    }
-    addBodyOverflow();
-    setShown(true);
-  };
-  const hideModal = () => {
-    setShown(false);
-    removeBodyOverflow();
-  };
-
+const ImageModal = ({ images }: ImageModalProps) => {
   return (
-    <>
-      <button onClick={showModal}>
-        <svg
-          data-darkreader-inline-stroke=""
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="1.5"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-hidden="true"
-          className="z-20 w-16 h-16 block text-white bg-gray-300 hover:bg-gray-500 bg-opacity-25 opacity-75 focus:outline-none rounded-lg absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:cursor-pointer"
+    <Gallery withDownloadButton>
+      {images.map(([imgUrl, { width, height }], index) => (
+        <Item
+          id={`image-${index}`}
+          key={index}
+          original={imgUrl}
+          width={width}
+          height={height}
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M9 9V4.5M9 9H4.5M9 9L3.75 3.75M9 15v4.5M9 15H4.5M9 15l-5.25 5.25M15 9h4.5M15 9V4.5M15 9l5.25-5.25M15 15h4.5M15 15v4.5m0-4.5l5.25 5.25"
-          ></path>
-        </svg>
-      </button>
-      <Modal
-        ref={ref}
-        show={shown}
-        onClose={hideModal}
-        size={"5xl"}
-        className={"!z-100"}
-      >
-        <Modal.Header>
-          <a href={downloadUrl}>
+          {({ open, ref }) => (
             <button
-              type="button"
-              className="focus:outline-none text-white bg-accent hover:bg-accent-600 focus:ring-4
-                            focus:ring-accent font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2
-                            dark:focus:ring-accent"
+              ref={ref as React.MutableRefObject<HTMLButtonElement>}
+              onClick={open}
             >
-              {i18n.t("forms.buttons.download")}
+              <svg
+                data-darkreader-inline-stroke=""
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                className="z-30 w-16 h-16 block text-white bg-gray-300 hover:bg-gray-500 bg-opacity-25 opacity-75 focus:outline-none rounded-lg absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:cursor-pointer"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9 9V4.5M9 9H4.5M9 9L3.75 3.75M9 15v4.5M9 15H4.5M9 15l-5.25 5.25M15 9h4.5M15 9V4.5M15 9l5.25-5.25M15 15h4.5M15 15v4.5m0-4.5l5.25 5.25"
+                ></path>
+              </svg>
             </button>
-          </a>
-        </Modal.Header>
-        <Modal.Body>
-          <span className="hidden" ref={ref}></span>
-          <div className="h-full flex items-center justify-center">
-            <img
-              src={imgUrl}
-              alt="car"
-              className="w-full h-full object-contain"
-            />
-          </div>
-        </Modal.Body>
-      </Modal>
-    </>
+          )}
+        </Item>
+      ))}
+    </Gallery>
   );
 };
 
