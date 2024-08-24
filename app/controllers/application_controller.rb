5777339eeb30b77b94c923ef4d1c82ec58fc248d
Dusan <dusanuveric@protonmail.com>
2024-08-24 15:38:59 +0200

    implement base json error handling
    Signed-off-by: Dusan <dusanuveric@protonmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,22 @@ class ApplicationController < ActionController::Base
 
     I18n.with_locale(locale, &action)
   end
+
+  def error_to_json(err)
+    case err
+    when ActiveRecord::RecordInvalid
+      attributes = err.record.errors.attribute_names
+      attributes.map do |attribute|
+        {
+          path: attribute.to_s,
+          messages: err.record.errors.full_messages_for(attribute),
+        }
+      end
+    when Dry::Validation::Result
+      err_hash = err.errors(full: true).to_h
+      err_hash.entries.map do |path, messages|
+        { path: path.to_s, messages: messages }
+      end
+    end
+  end
 end
